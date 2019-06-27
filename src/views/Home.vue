d75b3660c22d4dfe385f1b907ee04f5fff5a92c3
kota-matsui <matsui@ediplex.com>
2019-06-27 11:49:30 +0900

    format

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,34 +11,34 @@
       </div>
     </div>
     <div class="section">
-  <div class="box" v-for="(item, index) in result" :key="index">
-    <article class="media">
-      <div class="media-left">
-        <figure class="image is-64x64"> <img alt="Image" :src="item.artworkUrl100">
-        </figure>
-      </div>
-      <div class="media-content">
-        <div class="content">
-          <p> <strong> {{item.collectionName}} </strong> <small> {{item.artistName}} </small> <small> {{item.primaryGenreName}} </small>
-            <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-          </p>
-        </div>
-        <nav class="level">
-          <div class="level-left">
-            <a class="level-item">
-              <span class="icon is-small"> <i class="fa fa-reply"> </i> </span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small"> <i class="fa fa-retweet"> </i> </span>
-            </a>
-            <a class="level-item">
-              <span class="icon is-small"> <i class="fa fa-heart"> </i> </span>
-            </a>
+      <div class="box" v-for="(item, index) in result" :key="index">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64"> <img alt="Image" :src="item.artworkUrl100">
+            </figure>
           </div>
-        </nav>
+          <div class="media-content">
+            <div class="content">
+              <p> <strong> {{item.collectionName}} </strong> <small> {{item.artistName}} </small> <small> {{item.primaryGenreName}} </small>
+                <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+              </p>
+            </div>
+            <nav class="level">
+              <div class="level-left">
+                <a class="level-item">
+                  <span class="icon is-small"> <i class="fa fa-reply"> </i> </span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"> <i class="fa fa-retweet"> </i> </span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"> <i class="fa fa-heart"> </i> </span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </article>
       </div>
-    </article>
-  </div>
     </div>
   </div>
 </template>
